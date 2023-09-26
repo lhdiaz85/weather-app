7eafa838ee6122b4fe399d3502f00646a05e3d8f
lhdiaz85 <lhdiaz85@gmail.com>
2023-09-26 18:05:04 -0500

    Add JS template and API for weather forecast

--- src/index.js
+++ src/index.js
@@ -37,6 +37,88 @@ function formatDate(now) {
 let now = document.querySelector(".today");
 now.innerHTML = formatDate(new Date());
 
+//Forecast
+function showForecast(response) {
+  console.log(response.data);
+  let forecastElement = document.querySelector("#week-weather");
+  //let classNames = ["One", "Two", "Three", "Four", "Five", "Six", "Seven"];
+  let forecastHTML = "";
+  let days = ["Wed", "Thurs", "Fri", "Sat", "Sun", "Mon", "Tues"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+          <div class="accordion-item">
+            <div class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo"
+                aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo"
+                id="day1-button"
+              >
+                <div class="col-3">
+                  <h5 class="day1-title">${day}</h5>
+                </div>
+                <div class="col-4">
+                  <img
+                    id="day1-icon"
+                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                    alt=""
+                  />
+                </div>
+
+                <div class="col-2">
+                  <h6 class="day1-temp">
+                    <i class="fa-solid fa-arrow-up"></i>
+                    <span class="day1-high">80</span>°
+                  </h6>
+                </div>
+
+                <div class="col-2">
+                  <h6 class="day1-temp">
+                    <i class="fa-solid fa-arrow-down"></i>
+                    <span class="day1-low">75</span>°
+                  </h6>
+                </div>
+              </button>
+            </div>
+            <div
+              id="panelsStayOpen-collapseTwo"
+              class="accordion-collapse collapse"
+            >
+              <div class="accordion-body">
+                <div class="d-flex flex-row">
+                  <div class="col-4" id="feel-day1">
+                    <i class="fa-solid fa-temperature-empty"></i>
+                    <span class="icon-title">WEATHER</span>
+                    <br />
+                    <span id="day1-feel">scattered clouds</span>
+                  </div>
+                  <div class="col-4" id="humidity-day1">
+                    <i class="fa-solid fa-droplet"></i>
+                    <span class="icon-title">HUMIDITY</span>
+                    <br />
+                    <span id="day1-humidity">77</span>%
+                  </div>
+                  <div class="col-4" id="wind-day1">
+                    <i class="fa-solid fa-wind"></i>
+                    <span class="icon-title">WIND</span>
+                    <br />
+                    <span id="day1-windspeed">8</span> mph
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          `;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 //Current weather
 function showWeather(response) {
   //OpenWeather Data
@@ -85,7 +167,7 @@ function showWeather(response) {
 }
 
 //Search trigger
-function search(cityName) {
+function getCityCurrent(cityName) {
   //OpenWeather API
   //let keyOpenWeather = "0f8bc384a7c31b717a18cfe38a95ae06";
   //let unitOpenWeather = "imperial";
@@ -106,14 +188,28 @@ function search(cityName) {
     });
 }
 
-//City input
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityName = document.querySelector("#city-input").value;
-  search(cityName);
+function getCityForecast(cityName) {
+  let keySheCodes = "0ae8ed0af65d0db03aet4f1o89f6d9a8";
+  let unitSheCodes = "imperial";
+  let urlSheCodes = `https://api.shecodes.io/weather/v1/forecast?query=${cityName}&key=${keySheCodes}&units=${unitSheCodes}`;
+  console.log(urlSheCodes);
+  axios
+    .get(urlSheCodes)
+    .then(showForecast)
+    .catch((error) => {
+      console.log("Error fetching data from the SheCodes Weather API:", error);
+    });
 }
 
-function searchLocation(position) {
+//City input
+function findCity(event) {
+  event.preventDefault();
+  let cityName = document.querySelector("#city-input").value;
+  getCityCurrent(cityName);
+  getCityForecast(cityName);
+}
+
+function getLocationCurrent(position) {
   //OpenWeather API
   //let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
   //let unitName = "imperial";
@@ -134,9 +230,24 @@ function searchLocation(position) {
     });
 }
 
-function getCurrentLocation(event) {
+function getLocationForecast(position) {
+  //SheCodes Weather API
+  let keySheCodes = "0ae8ed0af65d0db03aet4f1o89f6d9a8";
+  let unitSheCodes = "imperial";
+  let urlSheCodes = `https://api.shecodes.io/weather/v1/forecast?lon=${position.coords.longitude}&lat=${position.coords.latitude}&key=${keySheCodes}&units=${unitSheCodes}`;
+  console.log(urlSheCodes);
+  axios
+    .get(urlSheCodes)
+    .then(showForecast)
+    .catch((error) => {
+      console.log("Error fetching data from the SheCodes Weather API:", error);
+    });
+}
+
+function findLocation(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
+  navigator.geolocation.getCurrentPosition(getLocationCurrent);
+  navigator.geolocation.getCurrentPosition(getLocationForecast);
 }
 
 function checkedFarenheit() {
@@ -167,10 +278,10 @@ function convertCelsius() {
 let farenheitTemperature = null;
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
+searchForm.addEventListener("submit", findCity);
 
 let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
+currentLocationButton.addEventListener("click", findLocation);
 
 let farenheitButton = document.querySelector("#btnradio1");
 farenheitButton.addEventListener("click", convertFarenheit);
@@ -179,4 +290,5 @@ let celsiusButton = document.querySelector("#btnradio2");
 celsiusButton.addEventListener("click", convertCelsius);
 
 //City default
-search("Chicago");
+getCityCurrent("Chicago");
+getCityForecast("Chicago");
